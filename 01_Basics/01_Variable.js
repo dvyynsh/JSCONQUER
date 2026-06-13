7459c39cb77f198a7076de771be749f60567b7bc
Divyansh Singh <divyansh205044@gmail.com>
2026-06-13 22:50:25 +0530

    Initial commit

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,15 @@
+const accountId = 14437
+let accountEmail = "divyansh205044@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let status
+
+console.log(accountId)
+
+/*
+prefer not to use var 
+because of issue in block scope and fuctional scope
+*/
+
+// we can also print all in once
+console.table([accountCity,accountEmail,accountId,accountPassword,status])
