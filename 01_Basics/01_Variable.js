8369065f7e4fd78e764988ea3b83a1e0d29e0eb2
Divyansh Singh <divyansh205044@gmail.com>
2026-06-16 21:22:29 +0530

    Update Variable.js

--- 01_Basics/01_Variable.js
+++ 01_Basics/01_Variable.js
@@ -1,16 +1,25 @@
-const accountId = 14437
-let accountEmail = "divyansh205044@gmail.com"
-var accountPassword = "12345"
-accountCity = "jaipur"
-let status
+// var se declare karege to do bar same variable create karne me No error Exp....
+// also scope ke bahar bhi access if(){} wale ke 
+var Yoo = "Divyansh"
+var Yoo = "Divyansh"
+console.log(Yoo)
 
-console.log(accountId)
+if(true){
+    var name = "Divyansh"
+}
+console.log(name)  // still prints
+// Old method
 
-/*
-prefer not to use var 
-because of issue in block scope and fuctional scope
-*/
 
-// we can also print all in once
-console.table([accountCity,accountEmail,accountId,accountPassword,status])
+// Variable ko kaise banate hai, but here let will not allow
+let name = "Divyansh"
+console.log(name)
+
+let age1 = 20
+age1 = 30
+console.log(age1, name)
+
+const age2 = 40
+console.log(age2, name)
+// Now you can't change
 
