8760d249c3a98ba318a3b938c08fbfe75f7330ee
Divyansh Singh <divyansh205044@gmail.com>
2026-06-17 17:13:27 +0530

    Add 02_Operator.js and update ToPushFile.md

---
+++ 01_Basics/02_Operator.js
@@ -0,0 +1,29 @@
+// Arithmetic Operator
+console.log(2+5)
+console.log(2-5)
+console.log(2*5)
+console.log(6/2)
+console.log(5%2)
+console.log(2**5)
+
+
+// Assigment operator
+let x = 20;
+let y = 10;
+x = x+y;
+console.log(x);
+
+
+// Comparison Operator
+let x = 10
+let y = "20"
+console.log(x>y)
+console.log(x>=y)
+console.log(x==y)      // True because JS converts string into number
+console.log(x===y)     // False
+// dono same type ke hone chaiye then ===, will return true
+let a = "abc45"
+let b = Number(a)
+console.log(b)         // return NaN -> Not a number because we can't convert mixture of string-number..
+
+
--- ToPushFile.md
+++ ToPushFile.md
@@ -2,3 +2,9 @@ git status
 git add .
 git commit -m "Update Variable.js"
 git push origin main
+
+
+
+cd "C:\Users\divya\OneDrive\Desktop\JavaScript\01_Basics"
+git remote add origin https://github.com/dvyynsh/JavaScript.git
+git push -u origin master:main
